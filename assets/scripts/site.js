df7e27294adb3a865a4353ec956eb657e267ac21
Nikhil Vytla <nvawesomeguy@gmail.com>
2020-08-06 22:52:31 -0700

    linking updates p11

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -22,18 +22,28 @@ $('.social-link').hover(
   function() {
     var getLink = $(this).data('platform');
     $('.social-url ul').css('opacity', '1');
-    $('.social-url-2 ul').css('opacity', '1');
     if (getLink == 'twitter') {
       $('.social-url ul').css('top', '0');
     } else if (getLink == 'instagram') {
       $('.social-url ul').css('top', '-24px');
-    } else if (getLink == 'linkedin') {
+    }
+  }, function() {
+    $('.social-url ul').css('opacity', '0');
+  }
+);
+
+// Header link 2 logic
+
+$('.social-link-2').hover(
+  function() {
+    var getLink = $(this).data('platform');
+    $('.social-url-2 ul').css('opacity', '1');
+    if (getLink == 'linkedin') {
       $('.social-url-2 ul').css('top', '0');
     } else if (getLink == 'github') {
       $('.social-url-2 ul').css('top', '-24px');
     }
   }, function() {
-    $('.social-url ul').css('opacity', '0');
     $('.social-url-2 ul').css('opacity', '0');
   }
 );
